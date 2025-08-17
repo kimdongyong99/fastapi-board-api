76e7e9f53ebacccb0ee3572c8da2364a63ed4152
kimdongyong99 <homeland007@naver.com>
2025-08-17 18:58:58 +0900

    ACCESS_TOKEN_EXPIRE_MINUTES 추가

--- app/core/config.py
+++ app/core/config.py
@@ -5,7 +5,7 @@ class Settings(BaseSettings):
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
     PROJECT_NAME: str = "FastAPI Board API"
-
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
     class Config:
         env_file = ".env"
         extra = "ignore"
